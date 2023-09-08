8c27b8cf7c195854dbfd678677874690f0be2951
resq-u <jakub12lepianka@wp.pl>
2023-09-08 19:22:16 +0200

    Add a function which changes color of the div over which mouse is hovered

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
-    const grid = document.createElement('div')
-    grid.classList.add('grid');
-    container.appendChild(grid);
-}
+function getGrid(){
+    for (let i = 0; i < 16; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('gridEl');
+        container.appendChild(gridElement);
+    }
+};
+
+getGrid();
+const grid = document.querySelectorAll('.gridEl');
+
+
+grid.forEach((element) => { 
+    element.addEventListener('mouseover', () => {
+        const coloredEl = document.querySelector('#colored');
+        element.setAttribute('id', 'colored');
+    });
+});
+
+//console.log(grid);
+
+/*
+grid.forEach((gridEl) => {
+    gridEl.addEventListener("mousover", (e) => console.log("helo"))
+});
+*/
