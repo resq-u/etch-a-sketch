6aa5f46104fb96069b4568fb753f1085b5a80416
resq-u <jakub12lepianka@wp.pl>
2023-09-09 20:52:40 +0200

    Add listening button which asks for desired grid size
    Put a function responsible for 'drawing' into a function startTracking which in turn contains a queryselector

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 const container = document.querySelector('#container');
 const btn = document.querySelector('button');
 
-function getGrid(){
-number = 32;
+function getGrid(number){
+if (number > 100) return;
 number *= number;
 
     for (let i = 0; i < number ; i++) {
         const gridElement = document.createElement('div')
         gridElement.classList.add('gridEl');
         container.appendChild(gridElement);
-        gridElement.style.cssText = `height: ${(number**(1/2)/number)*100}%; `;
+        gridElement.style.cssText = `height: ${(number**(1/2)/number)*100}%;`;
         gridElement.style.cssText = `width: ${(number**(1/2)/number)*100}%;`;
-
-    }
+    };
 };
 
-getGrid();
-const grid = document.querySelectorAll('.gridEl');
-
-grid.forEach((element) => { 
-    element.addEventListener('mouseover', () => {
-        const coloredEl = document.querySelector('#colored');
-        element.setAttribute('id', 'colored');
-    });
+btn.addEventListener('click', () => {
+    let wat = prompt("hej",);
+    getGrid(wat);
+    startTracking();
 });
+
+function startTracking() {
+
+    const grid = document.querySelectorAll('.gridEl');
+
+    grid.forEach((element) => { 
+        element.addEventListener('mouseover', () => {
+            const coloredEl = document.querySelector('#colored');
+            element.setAttribute('id', 'colored');
+        });
+    });
+
+}
+
